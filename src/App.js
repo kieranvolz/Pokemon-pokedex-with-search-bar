4ebb58432859bd69d211f1d426f7a2730781e1a9
Kieran Volz <kieran.volz@gmail.com>
2021-04-11 15:34:39 -0400

    cleaned everything up
    cleaned up my code and changed the react icon

--- src/App.js
+++ src/App.js
@@ -1,30 +1,10 @@
-// import logo from './logo.svg';
+
 import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//         <p>
-//           testing
-//         </p>
-//     </div>
-//   );
-// }
-
 import { useState } from "react";
 function App() {
   const [pokemonName, setPokemonName] = useState("ditto");
   const [chosen, setChosen] = useState(false);
   const [pokemonData, setPokemonData] = useState({
-    // name: "",
-    // species: "",
-    // img: "",
-    // hp: "",
-    // attack: "",
-    // defense: "",
-    // type: ""
-
-
     name: "",
     image: "",
     type: "",
@@ -41,14 +21,6 @@ function App() {
       .then((response) => {
         console.log(response);
         setPokemonData({
-          // name: pokemonName,
-          // species: response.species.name,
-          // img: response.sprites.front_default,
-          // hp: response.stats[0].base_stat,
-          // attack: response.stats[1].base_stat,
-          // defense: response.stats[3].base_stat,
-          // type: response.types[0].type.name
-
           name: response.name,
           image: response.sprites['front_default'],
           type: response.types.map((type) => type.type.name).join(', '),
@@ -86,39 +58,19 @@ function App() {
           <h1>Please choose a pokemon</h1>
         ) : (
           <>
-            {/* <h1>{pokemonData.name}</h1>
-            <img src={pokemonData.img} alt={pokemonData.name} />
-            <h2>{pokemonData.species}</h2>
-            <h2>{pokemonData.type}</h2>
-            <h2>{pokemonData.hp}</h2>
-            <h2>{pokemonData.attack}</h2>
-            <h2>{pokemonData.defense}</h2> */}
-
-
-
- 
-
-
-
         <div className="card pokemon">
-
 <img className="card-image"src={pokemonData.image} alt={pokemonData.name} />
-
             <h1 className="card-title" >#{pokemonData.id}</h1>
             <h1 className="card-title" id="PokemonName">{pokemonData.name}</h1>
             <p className="card-subtitle">Type: {pokemonData.type}</p>
             <p className="card-subtitle">Height: {pokemonData.height} ft</p>            
             <p className="card-subtitle">Weight: {pokemonData.weight} lbs</p>
-          
-          
           </div>
           </>
-        
         )}
       </div>
     </div>
   );
 }
-
 
 export default App;
